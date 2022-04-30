47532fb29de77a5e57c29ba07808b75ee457847c
jef <324976+xbrln@users.noreply.github.com>
2022-04-30 04:52:44 +0200

    Initialize project

---
+++ test/learn_integrant/core_test.clj
@@ -0,0 +1,7 @@
+(ns learn-integrant.core-test
+  (:require [clojure.test :refer :all]
+            [learn-integrant.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
